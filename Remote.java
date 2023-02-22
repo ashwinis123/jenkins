5571f176ab7133c94b91195e3ca7307dd7de91c9
Ashwini <ashwinigaikwad9019@gmail.com>
2023-02-22 08:46:01 +0530

    adding text to remote

--- Remote.java
+++ Remote.java
@@ -5,7 +5,7 @@ public class Remote {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		System.out.println("this is remote job chaining");
+		System.out.println("this is remote job");
 	}
 
 }
